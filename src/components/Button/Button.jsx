bbe6ff1aeb79b9347c60e924e5e2a60e2a94f659
Nadiia17 <nababiy@gmail.com>
2023-10-29 16:52:34 +0200

    api

--- src/components/Button/Button.jsx
+++
@@ -1,5 +0,0 @@
-import { StyledButton } from './Button.styled';
-
-export const Button = ({ onClick, children }) => (
-  <StyledButton onClick={onClick}>{children}</StyledButton>
-);
--- src/components/api.js
+++ src/components/api.js
@@ -1,16 +1,46 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const KEY = '39989925-901ad6a53913725b2cdfb60c2';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const KEY = 'a3b6d29cef5ec6a74db53e15ddd579d0';
 
-export const searchByQuery = async (query, page = 1) => {
+export const getTrending = async () => {
+  const response = await axios.get(`trending/movie/day?api_key=${KEY}`);
+  return response.data.results;
+};
+
+export const searchByQuery = async query => {
   const params = {
-    key: KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    per_page: 12,
-    page,
+    api_key: KEY,
+    query,
+    include_adult: false,
+    language: 'en-US',
   };
-  const response = await axios.get('', { params });
+  const response = await axios.get('search/movie', { params });
+  return response.data.results;
+};
+
+export const getMovieDetails = async movie_id => {
+  const params = {
+    api_key: KEY,
+    language: 'en-US',
+  };
+  const response = await axios.get(`movie/${movie_id}`, { params });
   return response.data;
+};
+
+export const getCastInfo = async movie_id => {
+  const params = {
+    api_key: KEY,
+    language: 'en-US',
+  };
+  const response = await axios.get(`movie/${movie_id}/credits`, { params });
+  return response.data.cast;
+};
+
+export const getReviews = async movie_id => {
+  const params = {
+    api_key: KEY,
+    language: 'en-US',
+  };
+  const response = await axios.get(`movie/${movie_id}/reviews`, { params });
+  return response.data.results;
 };
